28874a99ae94ca5cb934cd420f30540da099d6d9
Flowerafro <linehenriksen0@gmail.com>
2025-10-23 19:48:10 +0200

    styling for better header

--- cv-lineh/src/Components/Header.tsx
+++ cv-lineh/src/Components/Header.tsx
@@ -7,6 +7,8 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaBehance } from "react-icons/fa6";
 import { FaDeviantart } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
+import { MdOutlineWorkOutline } from "react-icons/md";
 import PDFButton from "./PDFButton";
 
 export default function Header() {
@@ -24,7 +26,7 @@ export default function Header() {
         <header className={`header-bg ${headerState} aurora-drift`}>
             {headerState === "fullscreen" ? (
                 <div className="intro-content">
-                    <h1> Hi!  Im Line a designer of digital experiences and web designs</h1>
+                    <h1> Hi! I'm Line, a designer of digital experiences and web designs</h1>
                     <button className="intro-btn" onClick={handleClick}> Welcome </button>
                 </div>
             ) : (
@@ -34,22 +36,24 @@ export default function Header() {
                             <img src={lhworks} alt="Profilbilde" className="profile-img" />
                         </NavLink>
                         <div className="profile-info">
-                            <span className="profile-name">Line H</span>
+                            <p className="profile-name"><span>Line</span>Henriksen</p>
                             <span className="profile-title">
-                                <GoMail />
-                                <a href="mailto:linehenriksen0@gmail.com">Contact me!</a>
+                                <a href="mailto:linehenriksen0@gmail.com"><GoMail /></a>
+                                <a href="tel:+4748222033" aria-label="call Line H"><IoCall /></a>
                             </span>
                         </div>
+                                                <PDFButton filePath="/application-cv/Resume2025-2.pdf" title="My CV" className="cv-button" />
                     </div>
-                    <div className="contact-links">
-                        <ul>
-                        <li><a href="https://github.com/Flowerafro" target="_blank" rel="noopener noreferrer"><FaGithub /></a></li>
-                        <li><a href="https://www.linkedin.com/in/line-henriksen-542a44290/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a></li>
-                        <li><a href="https://www.behance.net/linehenriksen2" target="_blank" rel="noopener noreferrer"><FaBehance /></a></li>
-                        <li><a href="https://www.deviantart.com/fl0werafr0" target="_blank" rel="noopener noreferrer"><FaDeviantart /></a></li>
-                    </ul>
-                    </div>
-                    <PDFButton filePath="/application-cv/Resume2025-2.pdf" title="My CV" className="cv-button" />
+                        <div className="contact-links">
+                            <ul>
+                                <li><a href="https://github.com/Flowerafro" target="_blank" rel="noopener noreferrer"><FaGithub /></a></li>
+                                <li><a href="https://www.linkedin.com/in/line-henriksen-542a44290/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a></li>
+                                <li><a href="https://www.behance.net/linehenriksen2" target="_blank" rel="noopener noreferrer"><FaBehance /></a></li>
+                                <li><a href="https://www.deviantart.com/fl0werafr0" target="_blank" rel="noopener noreferrer"><FaDeviantart /></a></li>
+                                <li><a href="https://uffiulf.github.io/syntax-flow2/#profile" target="_blank" rel="noopener noreferrer"><MdOutlineWorkOutline /></a></li>
+                            </ul>
+                        </div> 
+                   
                     <div className="colortogglediv">
                         <ColorModeToggle />
                     </div>
